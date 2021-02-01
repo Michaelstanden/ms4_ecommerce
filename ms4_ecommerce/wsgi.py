f5d1e796362f8929531ed998a34431c59862a88e
Michaelstanden <michaelstanden@hotmail.com>
2021-02-01 15:10:11 +0000

    initial commit

---
+++ ms4_ecommerce/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ms4_ecommerce project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ms4_ecommerce.settings')
+
+application = get_wsgi_application()
