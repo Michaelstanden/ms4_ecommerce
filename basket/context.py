da0c570a670f6355d4d1c59723a9b0a3c0df6b10
Michaelstanden <michaelstanden@hotmail.com>
2021-02-11 11:15:56 +0000

    added form for basket qauntity and add to basket bttn. Added python templating to products.html. Added view.py in in products.html. Added view.py in basket app and urls.py basket functionality

---
+++ basket/context.py
@@ -0,0 +1,38 @@
+from django.shortcuts import get_object_or_404
+from products.models import Product
+from decimal import Decimal
+
+
+def basket_contents(request):
+
+    basket_items = []
+    total = 0
+    product_count = 0
+    basket = request.session.get('basket', {})
+
+    for item_id, item_data in basket.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            basket_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for quantity in item_data.items():
+                total += quantity * product.price
+                product_count += quantity
+                basket_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                })
+    context = {
+        'basket_items': basket_items,
+        'total': total,
+        'product_count': product_count,
+    }
+    return context
--- ms4_ecommerce/settings.py
+++ ms4_ecommerce/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 "django.template.context_processors.media",
                 'django.contrib.messages.context_processors.messages',
+                'basket.context.basket_contents',
             ],
         },
     },
@@ -150,3 +151,4 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
