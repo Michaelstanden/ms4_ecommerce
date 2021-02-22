2db83dd41a2becec84642b445742c71f67c7509c
Michaelstanden <michaelstanden@hotmail.com>
2021-02-22 16:35:06 +0000

    added basket.html rendering for products to display images, details and prices of items to user

--- basket/views.py
+++ basket/views.py
@@ -10,12 +10,6 @@ def view_basket(request):
 
 from django.shortcuts import render, redirect
 
-# Create your views here.
-
-def view_bag(request):
-    """ A view that renders the bag contents page """
-
-    return render(request, 'bag/bag.html')
 
 def add_to_basket(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
