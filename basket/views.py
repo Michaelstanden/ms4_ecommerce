14ca00b8fef4cdaf199110563008e64061aed096
Michaelstanden <michaelstanden@hotmail.com>
2021-02-25 08:40:03 +0000

    reworked js script in views.py to ensure function to adjust basket worked

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import (render, redirect,
+                              reverse, HttpResponse, get_object_or_404)
+from django.contrib import messages
+from products.models import Product
 
 
 # Create your views here.
@@ -14,35 +17,36 @@ def add_to_basket(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    product = get_object_or_404(Product, pk=item_id)
     basket = request.session.get('basket', {})
 
     if item_id in list(basket.keys()):
         basket[item_id] += quantity
+        messages.success(request, f'Updated {product.name}to{basket[item_id]}')
     else:
         basket[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your basket')
 
     request.session['basket'] = basket
     print(request.session['basket'])
     return redirect(redirect_url)
 
-    def adjust_basket(request, item_id):
-        """Adjust the quantity of the specified product to the specified amount"""
 
+def adjust_basket(request, item_id):
+    """Adjust the quantity of the specified product to the
+    specified amount and display appropriate message"""
+
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     if request.POST:
         basket = request.session.get('basket', {})
 
         if quantity > 0:
             basket[item_id] = quantity
-        else:
-            del basket[item_id]
-            if not basket[item_id]:
-                basket.pop(item_id)
-    else:
-        if quantity > 0:
-            basket[item_id] = quantity
+            messages.success(request, f'Updated {product.name} quantity to {basket[item_id]}')
         else:
             basket.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your basket')
 
     request.session['basket'] = basket
     return redirect(reverse('view_basket'))
