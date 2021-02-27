8ce5dcb1b7d8c7727b03c4b454727f0d758804ff
Michaelstanden <michaelstanden@hotmail.com>
2021-02-27 20:57:22 +0000

    added toasts folder and toastmessages.html code along with views.oy in basket and js block code to call toasts in base.html

--- basket/views.py
+++ basket/views.py
@@ -14,10 +14,9 @@ def view_basket(request):
 
 def add_to_basket(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
-
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    product = get_object_or_404(Product, pk=item_id)
     basket = request.session.get('basket', {})
 
     if item_id in list(basket.keys()):
