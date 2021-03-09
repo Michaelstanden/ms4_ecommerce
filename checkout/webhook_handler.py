5a6b7ed83d834057da8cdd8160c89c4a73707cf7
Michaelstanden <michaelstanden@hotmail.com>
2021-03-09 21:16:44 +0000

    added confirmation email templates in checkout and adjusted typo on cheout html bakset to basket

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -91,7 +91,7 @@ class StripeWH_Handler:
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
                     postcode=shipping_details.address.postal_code,
-                    town_or_city=shipping_details.address.city,
+                    city=shipping_details.address.city,
                     street_address1=shipping_details.address.line1,
                     street_address2=shipping_details.address.line2,
                     county=shipping_details.address.state,
