8678f8d662d424024755a92ce26e447ef3270337
Michaelstanden <michaelstanden@hotmail.com>
2021-03-10 15:35:32 +0000

    added views.py error message function if form is filled out incorrectly in add products function and also updated views.py in profiles to throw error message if user adds incorrect information or misses a field

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
 from .forms import UserProfileForm
@@ -7,17 +8,22 @@ from .forms import UserProfileForm
 from checkout.models import Order
 
 
+@login_required
 def profile(request):
-    """ Display the user's profile. """
+    """Display the user's profile"""
+
     profile = get_object_or_404(UserProfile, user=request.user)
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
+            messages.success(request, 'Profile updated succesfully')
+        else:
+            messages.error(request,
+                           'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
@@ -44,4 +50,4 @@ def order_history(request, order_number):
         'from_profile': True,
     }
 
-    return render(request, template, context)
+    return render(request, template, context
