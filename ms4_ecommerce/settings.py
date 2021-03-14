d52d8788c0c6c559dfc40c52a8413abbb3e47848
Michaelstanden <michaelstanden@hotmail.com>
2021-03-14 18:21:26 +0000

    added heroku app variable in env.py and connected to database in stettings.py

--- ms4_ecommerce/settings.py
+++ ms4_ecommerce/settings.py
@@ -12,6 +12,8 @@ from os import path
 if path.exists("env.py"):
     import env
 
+import dj_database_url    
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -117,14 +119,18 @@ WSGI_APPLICATION = 'ms4_ecommerce.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
