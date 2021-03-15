37b94724575d0a9aec173e0bd9ef3d31ece70695
Michaelstanden <michaelstanden@hotmail.com>
2021-03-15 21:47:44 +0000

    deploy to heroku

--- ms4_ecommerce/settings.py
+++ ms4_ecommerce/settings.py
@@ -12,7 +12,7 @@ from os import path
 if path.exists("env.py"):
     import env
 
-import dj_database_url    
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
