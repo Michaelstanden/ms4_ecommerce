8fc9fdec7c1374b5c8f4b0ea3ffcfb7b722734e9
Michaelstanden <michaelstanden@hotmail.com>
2021-03-20 20:36:52 +0000

    added my email gmail account to see if this will fix password reset error

--- ms4_ecommerce/settings.py
+++ ms4_ecommerce/settings.py
@@ -206,7 +206,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'paperpals@example.com'
+    DEFAULT_FROM_EMAIL = 'michaelstanden1990@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
